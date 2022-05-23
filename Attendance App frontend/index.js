878e98e39797d8217665ab26d1f93805935b0a1f
shivam <shivamgokarankar86@gmail.com>
2022-05-23 08:14:30 +0530

    Attendance web app front end sourcecode

---
+++ Attendance App frontend/index.js
@@ -0,0 +1,8 @@
+var element1=document.getElementById('login-button');
+
+function showLoginForm()
+{
+    var element=document.getElementById('form-container');
+        element.setAttribute("style","display:block;transition:ease-in-out 2s;");
+
+}
