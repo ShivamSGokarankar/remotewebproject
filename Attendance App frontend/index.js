b62e6bc989b34984c06176c96b70cbeede6e3bfd
shivam <shivamgokarankar86@gmail.com>
2022-05-24 00:03:46 +0530

    sign up form added with confirm password functionality

--- Attendance App frontend/index.js
+++ Attendance App frontend/index.js
@@ -1,28 +1,39 @@
-var element1=document.getElementById('login-button');
 
 function showLoginForm()
 {
-    var element=document.getElementById('form-container');
-        element.setAttribute("style","display:block;transition:ease-in-out 2s;");
+    document.getElementById('form-container').setAttribute("style","display:block;transition:ease-in-out 2s;");
 
 }
-
 function showpassword()
 {
-    var element1=document.getElementById('password-field');
-    // if(element1.type==='password')
-    // {
-    //     element1.type='text';
-    // }
-    // else
-    // {
-    //     element1.type='password';
-
-    // }
-    element1.type = 'text';
+    var element=document.getElementById('password-field');
+    element.type = 'text';
 }
 
 function hidepassword() {
-    var element1=document.getElementById('password-field');
-    element1.type = 'password';
+    var element=document.getElementById('password-field');
+    element.type = 'password';
+}
+function hideConfirmPassword()
+{
+    var element=document.getElementById('Confirm-password')
+    element.type='password';
+}
+function showConfirmPassword()
+{
+    var element=document.getElementById('Confirm-password')
+    element.type='text';
+}
+function matchPassword()
+{
+    var pass1=document.getElementById('password').value;
+    var pass2 =document.getElementById('Confirm-password').value;
+    var color=document.getElementById('Confirm-password').style.borderColor;
+    if(pass1!==pass2 && pass2!='')
+    {
+        document.getElementById('Confirm-password').style.borderColor= 'red';
+    }
+    else{
+        document.getElementById('Confirm-password').style.borderColor= '';
+    }
 }
