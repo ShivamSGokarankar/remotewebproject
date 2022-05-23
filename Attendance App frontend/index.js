32759e04d05c2ca8f3ddd360eb5110fbc0ed9e72
shivam <shivamgokarankar86@gmail.com>
2022-05-23 19:27:37 +0530

    password show/hide icon added

--- Attendance App frontend/index.js
+++ Attendance App frontend/index.js
@@ -6,3 +6,23 @@ function showLoginForm()
         element.setAttribute("style","display:block;transition:ease-in-out 2s;");
 
 }
+
+function showpassword()
+{
+    var element1=document.getElementById('password-field');
+    // if(element1.type==='password')
+    // {
+    //     element1.type='text';
+    // }
+    // else
+    // {
+    //     element1.type='password';
+
+    // }
+    element1.type = 'text';
+}
+
+function hidepassword() {
+    var element1=document.getElementById('password-field');
+    element1.type = 'password';
+}
